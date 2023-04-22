7615a1d0c3af7cfc5e84e0812946863595da7867
vish0504 <122441761+vish0504@users.noreply.github.com>
2023-04-22 18:17:40 +0530

    initial message

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import SlotM from "./SlotMach";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="heading_style">
+        🎰 Welcome to
+        <span style={{ fontWeight: "bold" }}> Slot machine game</span> 🎰
+      </h1>
+      <div className="slotmachine">
+        <SlotM x="😄" y="😄" z="😄" />
+        <hr />
+        <SlotM x="👶" y="😄" z="⚜️" />
+        <hr />
+        <SlotM x="🍎" y="🍌" z="🍊" />
+        <hr />
+        <SlotM x="🚯" y="😄" z="😄" />
+        <hr />
+        <SlotM x="😄" y="😄" z="😄" />
+        <hr />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/SlotMach.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const SlotM = (props) => {
+  //   let x = "😄";
+  //   let y =  "😄";
+  //   let z = "🎅";
+
+  let x = props.x;
+  let y = props.y;
+  let z = props.z;
+
+  if (x === y && y === z) {
+    return (
+      <>
+        <div className="solt_inner">
+          <h1>
+            {x} {y} {z}
+          </h1>
+          <h1> This is Matching. </h1>
+          <hr />
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="solt_inner">
+          <h1>
+            {x} {y} {z}
+          </h1>
+          <h1> This is Not Matching. </h1>
+          <hr />
+        </div>
+      </>
+    );
+  }
+};
+
+export default SlotM;
